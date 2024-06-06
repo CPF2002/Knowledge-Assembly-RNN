05b235877c3c2941466ff3c6c11a031fff4d48d6
Compton French <compton.french@gmail.com>
2024-06-06 15:27:39 -0700

    Update main_no_train.py

--- main_no_train.py
+++ main_no_train.py
@@ -36,15 +36,16 @@ if __name__ == '__main__':
     # set up dataset and network hyperparams (optionally via command line)
     args, device, multiparams = mnet.define_hyperparams()
     args.all_fullrange = False      # False = blocked; True = interleaved
+    args.which_context = 0          # 0 = all contexts; 1 = LOWR (low range context); 2 = HIGHR (high range context)
     args.train_lesion_freq = 0.1    # 0.0 or 0.1  (also 0.2, 0.3, 0.4 for blocked & true context case)
     args.block_int_ttsplit = False  # True: test on a different distribution (block/interleave) than training
     args.retrain_decoder = False
     #args.model_id = 9999          # for visualising or analysing a particular trained model
 
     # Train a network from scratch and save it
-    #print('Training network...')
-    #mnet.train_and_save_network(args, device, multiparams)
-    #print('Training complete and network saved. main')
+    # print('Training network...')
+    # mnet.train_and_save_network(args, device, multiparams)
+    # print('Training complete and network saved. main')
 
     # Analyse the trained network (extract and save network activations)
     print('\nAnalysing network...')
@@ -58,20 +59,26 @@ if __name__ == '__main__':
     print('\nGenerating plots...')
     MDS_dict, args = anh.average_activations_across_models(args)
     mplt.generate_plots(MDS_dict, args)  # (Figure 3 + extras)
-
+    
+    if args.all_fullrange:
+      print('\nTraining Curricula: Interleaved')
+    else:
+      print('\nTraining Curricula: Blocked')
+    print('\nWhich Context: ', args.which_context)
+      
     # Plot the lesion test performance
-    print('\nPlotting lesion tests...')
-    mplt.perf_vs_context_distance(args, device)     # Assess performance after a lesion vs context distance (Figure 2 and S1)
-    mplt.compare_lesion_tests(args, device)      # compare the performance across the different lesion frequencies during training (Figure 2)
+    #print('\nPlotting lesion tests...')
+    #mplt.perf_vs_context_distance(args, device)     # Assess performance after a lesion vs context distance (Figure 2 and S1)
+    #mplt.compare_lesion_tests(args, device)      # compare the performance across the different lesion frequencies during training (Figure 2)
 
     # Statistical tests: is network behaviour better fit by an agent using the local-context or global-context policy
-    print('\nStatistical tests...')
-    anh.model_behaviour_vs_theory(args, device)
+    #print('\nStatistical tests...')
+    #anh.model_behaviour_vs_theory(args, device)
 
     # Load representations and check cross-line big/small generalisation
-    print('\nCross-line generalisation...')
-    anh.cross_line_rep_generalisation(args)
-    anh.cross_line_rep_generalisation_human(args)
+    #print('\nCross-line generalisation...')
+    #anh.cross_line_rep_generalisation(args)
+    #anh.cross_line_rep_generalisation_human(args)
 
     # Load a trained network (no VI), freeze the first layer (recurrent) weights and then retrain the decoder with VI and save it
     #print('\nRetraining decoder...')
